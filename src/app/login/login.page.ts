7cb1a79e1c45b66bf12adf2f2f042617449c29c9
Iliya Sobolev <soboli@att.net>
2020-07-13 14:15:12 -0500

    Added login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { User } from '../user.model';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  user:User = new User();
+
+  constructor(
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
-  }
 
+    this.user.username = 'testuser5';
+    this.user.password = 'password';
+    this.authService.logIn(this.user).subscribe(
+      (response:any) => {
+       console.log(response);
+      }
+    );
+
+    this.authService.logOut().subscribe(
+      (response:any) => {
+       console.log(response);
+      }
+    );
+
+    this.authService.register(this.user).subscribe(
+      (response:any) => {
+       console.log(response);
+      }
+    );
+
+  }
 }
