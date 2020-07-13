ea57a895fcf33494cfd555654394dfb30b3ac156
Iliya Sobolev <soboli@att.net>
2020-07-13 16:49:40 -0500

    Added registration

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../users.service';
+import { AuthService } from '../auth.service';
+import { User } from '../user.model';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
@@ -6,10 +11,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  user:User = new User();
+  errors: any = {};
+  errorMessage: string;
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit()  {}
+  response(response): void{
+    if(response.success===false){
+      this.errorMessage = 'Invalid Credentials';
+    }
+  }
+
+  // this.authService.register(this.user).subscribe(
+  //   (response:any) => {
+  //    console.log(response);
+  //   }
+  // );
+  public onSubmit(): void{
+    this.authService.register(this.user).subscribe(
+      (response:any) => {
+       this.response(response);
+      }
+    );
   }
 
 }
